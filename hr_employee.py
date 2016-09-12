e0011887ef3ce2d39073db3f24680b0aa9cc1fc7
Juned <juned.tan@gmail.com>
2016-09-12 18:38:35 +0700

    wip perbaikan di laptop Juned

--- hr_employee.py
+++ hr_employee.py
@@ -82,6 +82,15 @@ class hr_employee(osv.osv):
 	# panggil create biasa
 		return super(hr_employee, self).create(cr, uid, vals, context)
 	
+	def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+		if not args: args = []
+		if not context: context = {}
+		if name:
+			ids = self.search(cr, uid, ['|',('name', operator, name),('emp_no', operator, name)] + args, limit=limit, context=context)
+		else:
+			ids = self.search(cr, uid, args, limit=limit, context=context)
+		return self.name_get(cr, uid, ids, context)
+		
 # CRON --------------------------------------------------------------------------------------------------------------------------
 	
 	def cron_employee_work_year(self, cr, uid, context=None):

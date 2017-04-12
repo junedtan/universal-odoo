34c469434f74346a2a7e5d73721cbe1f97b1c6bc
Juned <juned.tan@gmail.com>
2017-04-12 12:19:07 +0700

    membuat jadi satu baris help untuk config

--- res_config.py
+++ res_config.py
@@ -7,14 +7,8 @@ class universal_config_settings(osv.osv_memory):
 
 	_columns = {
 		'default_first_party_name': fields.char("Contract's First Party Name", size=100, default_model="hr.contract", 
-			help="""
-				When printing out contract there is a section for signing up containing first party (representing company) and second party (the employee). 
-				This setting is for name of the first party.
-			"""),
+			help="When printing out contract there is a section for signing up containing first party (representing company) and second party (the employee). This setting is for name of the first party."),
 		'default_first_party_position': fields.char("Contract's First Party Position", size=100, default_model="hr.contract", 
-			help="""
-				When printing out contract there is a section for signing up containing first party (representing company) and second party (the employee). 
-				This setting is for position of the first party.
-			"""),
+			help="When printing out contract there is a section for signing up containing first party (representing company) and second party (the employee). This setting is for position of the first party."),
 	}
 	

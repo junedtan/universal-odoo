d1fa29d879ba48ee9fc08bfd58cb2f8dd60b682c
sammyh27 <sammyh27@gmail.com>
2017-04-18 11:21:31 +0700

    Contract: ketika Schedule Shuttle, filter vehicle berdasarkan hasil fleet planning contract

--- foms_contract.py
+++ foms_contract.py
@@ -860,20 +860,30 @@ class foms_contract_shuttle_schedule_line_memory(osv.osv):
 	_name = "foms.contract.shuttle.schedule.line.memory"
 	_description = 'Contract Shuttle Schedule Line'
 	
-	def _contract_car_drivers_fleet_vehicle(self, cr, uid, ids, context=None):
+	# def _contract_car_drivers_fleet_vehicle(self, cr, uid, contract_id, context=None):
+	# # filter vehicle berdasarkan hasil fleet planning contract
+	# 	shuttle_schedule_obj = self.pool.get('foms.contract.shuttle.schedule.memory')
+	# 	contract_obj = self.pool.get('foms.contract')
+	# 	result = {}
+	# 	for shuttle_schedule_line in self.browse(cr, uid, ids, context):
+	# 		shuttle_schedule = shuttle_schedule_obj.browse(cr, uid, shuttle_schedule_line.header_id.id, context)
+	# 		contract = contract_obj.browse(cr, uid, shuttle_schedule.contract_id.id, context)
+	# 		fleet_vehicle_id = []
+	# 		for car_driver in contract.car_drivers:
+	# 			fleet_vehicle_id.append(car_driver.fleet_vehicle_id)
+	# 		result[shuttle_schedule.id] = fleet_vehicle_id
+	# 	return result
+	
+	def _contract_car_drivers_fleet_vehicle(self, cr, uid, ids, contract_id, context=None):
 		# filter vehicle berdasarkan hasil fleet planning contract
-		shuttle_schedule_obj = self.pool.get('foms.contract.shuttle.schedule.memory')
 		contract_obj = self.pool.get('foms.contract')
-		result = {}
-		for shuttle_schedule_line in self.browse(cr, uid, ids, context):
-			shuttle_schedule = shuttle_schedule_obj.browse(cr, uid, shuttle_schedule_line.header_id.id, context)
-			contract = contract_obj.browse(cr, uid, shuttle_schedule.contract_id.id, context)
-			fleet_vehicle_id = []
-			for car_driver in contract.car_drivers:
-				fleet_vehicle_id.append(car_driver.fleet_vehicle_id)
-			result[shuttle_schedule.id] = fleet_vehicle_id
-		return result
-		
+		contract = contract_obj.browse(cr, uid, contract_id, context)
+		fleet_vehicle_id = []
+		for car_driver in contract.car_drivers:
+			fleet_vehicle_id.append(car_driver.fleet_vehicle_id.id)
+		return {'domain':{'fleet_vehicle_id':[('id','in',fleet_vehicle_id)]}}
+		# return fleet_vehicle_id
+	
 # COLUMNS ------------------------------------------------------------------------------------------------------------------
 
 	_columns = {
@@ -889,11 +899,11 @@ class foms_contract_shuttle_schedule_line_memory(osv.osv):
 			('6','Sunday'),], 'Day of Week', required=True),
 		'sequence': fields.integer('Sequence', required=True),
 		'route_id': fields.many2one('res.partner.route', 'Route', ondelete='restrict', required=True),
-		'fleet_vehicle_id': fields.many2one('fleet.vehicle', 'Fleet Vehicle', ondelete='restrict', required=True,
-			domain="[('id', 'in', contract_car_drivers_fleet_vehicle.ids)]"),
+		# 'contract_car_drivers_fleet_vehicle': fields.function(_contract_car_drivers_fleet_vehicle, method=True, type='one2many'),
+		'fleet_vehicle_id': fields.many2one('fleet.vehicle', 'Fleet Vehicle', ondelete='restrict', required=True,),
+			# domain="[('id', 'in', contract_car_drivers_fleet_vehicle.ids)]"),
 		'departure_time': fields.float('Departure Time', required=True),	
 		#'arrival_time': fields.float('Arrival Time', required=True),
-		'contract_car_drivers_fleet_vehicle': fields.function(_contract_car_drivers_fleet_vehicle, method=True, type='one2many'),
 	}
 	
 # ==========================================================================================================================

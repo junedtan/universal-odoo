4d0af2e88b267f1c3f6003fc107875b2ca6e1d0d
aaelsldehoc <aaelsldehoc@gmail.com>
2017-04-26 00:39:04 +0700

    no message

--- foms_order.py
+++ foms_order.py
@@ -1103,13 +1103,13 @@ class foms_order(osv.osv):
 			if len(first_order) == 0:
 				order_pass_day = self._get_running_multiday_orders(cr, uid, driver.id, yesterday)
 				if len(order_pass_day) > 0:
-					working_time_leaf = self._get_contract_working_time(order_pass_day.customer_contact_id, yesterday)
+					working_time_leaf = self._get_contract_working_time(order_pass_day.customer_contract_id, yesterday)
 				#dapetin working time
 					date_clock_in, date_clock_out = self._determine_clock_datetimes(cr, uid, None, None, yesterday,
 						working_time_leaf.working_time_type, True, working_time_leaf.hour_from, working_time_leaf.hour_to, working_time_leaf.max_hour)
-					if len(first_clock_in) == 0 and len(date_clock_in) != 0:
+					if len(first_clock_in) == 0 and date_clock_in is not None:
 						self._create_attendance(cr, uid, driver.id, last_order.customer_contract_id.id, 'sign_in', date_clock_in, last_order.id)
-					if len(last_clock_out) == 0 and len(date_clock_out) != 0:
+					if len(last_clock_out) == 0 and date_clock_out is not None:
 						self._create_attendance(cr, uid, driver.id, last_order.customer_contract_id.id, 'sign_out', date_clock_out, last_order.id)
 			else:
 				working_time_leaf = self._get_contract_working_time(first_order.customer_contract_id, yesterday)

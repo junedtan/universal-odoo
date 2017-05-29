58caa00b10a28fe5cf10023d1671ae1d0f1295f8
sammyh27 <sammyh27@gmail.com>
2017-05-29 13:11:03 +0700

    timeline DONE

--- controllers/timeline.py
+++ controllers/timeline.py
@@ -97,3 +97,96 @@ class universal_timeline(osv.osv):
 			'drivers': result,
 			'hours': hours,
 		}
+	
+	
+	def get_timeline_by_driver(self, cr, uid, driver_id, start_date_string, end_date_string):
+		user_obj = self.pool.get('res.users')
+		order_obj = self.pool.get('foms.order')
+		start_date = datetime.strptime(start_date_string,'%Y-%m-%d')
+		end_date = datetime.strptime(end_date_string,'%Y-%m-%d')
+		
+		driver_data = user_obj.browse(cr, uid, driver_id)
+		result = []
+		while start_date <= end_date:
+			tomorrow_date = start_date + timedelta(days=1)
+		# GET PLANNED AND ACTUAL ORDER
+			planned_order_ids = order_obj.search(cr, uid, [
+				('state', 'not in', ['canceled', 'rejected']),
+				('assigned_driver_id.user_id', '=', driver_data.id),
+				('start_planned_date', '<', (tomorrow_date - timedelta(hours=7)).strftime("%Y-%m-%d %H:%M:%S")),
+				('finish_planned_date', '>=', (start_date - timedelta(hours=7)).strftime("%Y-%m-%d %H:%M:%S")),
+			], order='start_planned_date ASC')
+			actual_order_ids = order_obj.search(cr, uid, [
+				('state', 'not in', ['canceled', 'rejected']),
+				('actual_driver_id.user_id', '=', driver_data.id),
+				('start_date', '<', (tomorrow_date - timedelta(hours=7)).strftime("%Y-%m-%d %H:%M:%S")),
+				'|',('finish_date', '>=', (start_date - timedelta(hours=7)).strftime("%Y-%m-%d %H:%M:%S")), ('finish_date', '=', False),
+			], order='start_date ASC')
+		# ACTUAL ORDER
+			actual_orders = []
+			finish_before = 0
+			for order_actual in order_obj.browse(cr, uid, actual_order_ids):
+				start = datetime.strptime(order_actual.start_date,'%Y-%m-%d %H:%M:%S') + timedelta(hours=7)
+				if order_actual.finish_date:
+					finish = datetime.strptime(order_actual.finish_date,'%Y-%m-%d %H:%M:%S') + timedelta(hours=7)
+				else:
+					finish = datetime.now()
+				start_minute = 0
+				finish_minute = 24 * 60
+				if start > start_date:
+					start_minute = start.hour * 60 + start.minute
+				if finish < tomorrow_date:
+					finish_minute = finish.hour * 60 + finish.minute
+				actual_orders.append({
+					'start': start_minute - finish_before,
+					'finish': finish_minute - start_minute,
+				})
+				finish_before = finish_minute
+		# PLANNED ORDER
+			planned_orders = []
+			finish_before = 0
+			for order_planned in order_obj.browse(cr, uid, planned_order_ids):
+				start = datetime.strptime(order_planned.start_planned_date,'%Y-%m-%d %H:%M:%S') + timedelta(hours=7)
+				finish = datetime.strptime(order_planned.finish_planned_date,'%Y-%m-%d %H:%M:%S') + timedelta(hours=7)
+				start_minute = 0
+				finish_minute = 24 * 60
+				if start > start_date:
+					start_minute = start.hour * 60 + start.minute
+				if finish < tomorrow_date:
+					finish_minute = finish.hour * 60 + finish.minute
+				planned_orders.append({
+					'start': start_minute - finish_before,
+					'finish': finish_minute - start_minute,
+				})
+				finish_before = finish_minute
+		# ADD TO RESULT
+			result.append({
+				'date_string': start_date.strftime("%d-%m-%Y"),
+				'planned_orders': planned_orders,
+				'actual_orders': actual_orders
+			})
+			start_date += timedelta(days=1);
+		hours = []
+		for hour in range(0, 24):
+			hours.append(str(hour).zfill(2) + ':00')
+		return {
+			'status': 'ok',
+			'driver_name': driver_data.name,
+			'dates': result,
+			'hours': hours,
+		}
+	
+	def get_drivers(self, cr, uid):
+		user_obj = self.pool.get('res.users')
+		driver_ids = user_obj.get_user_ids_by_group(cr, SUPERUSER_ID, 'universal', 'group_universal_driver')
+		result = []
+		for driver_data in user_obj.browse(cr, uid, driver_ids):
+			result.append({
+				'id': driver_data.id,
+				'name': driver_data.name,
+			})
+		result = sorted(result, key=lambda driver: driver['name'])
+		return {
+			'status': 'ok',
+			'drivers': result,
+		}
--- static/src/js/timeline.js
+++ static/src/js/timeline.js
@@ -65,8 +65,105 @@ openerp.universal = function(instance) {
         		}).done(function(response) {
         			console.log(response);
         			if (response.status) {
-						$('#div_main').html(self.QWeb.render("universal_timelie_by_date_table",{
+						$('#div_main').html(self.QWeb.render("universal_timeline_by_date_table",{
 							'drivers': response.drivers,
+							'hours': response.hours,
+						}));
+					} else {
+						alert('Server Unreachable.')
+					}
+        		});
+		},
+
+		pad: function(num, size){
+			var s = num+"";
+			while (s.length < size) s = "0" + s;
+			return s;
+		},
+
+	});
+
+// TIMELINE BY DRIVER =======================================================================================================
+
+	instance.universal.TimelineByDriver = instance.web.Widget.extend({
+
+		QWeb: QWeb,
+		_t: _t,
+		_lt: _lt,
+		instance: instance,
+		className: 'oe_universal_timeline_by_driver',
+		driver_id: 0,
+
+		events: {
+			'click #button_filter': 'onclick_filter_button',
+		},
+
+// FUNDAMENTAL FUNCTION -----------------------------------------------------------------------------------------------------
+
+		init: function(parent) {
+			this._super(parent);
+			var context = arguments[1]['context'];
+			this.force_readonly = (context['force_readonly'] === 1);
+			if (context['hide_sidebar']) {
+				$('.oe_leftbar').hide();
+			}
+		},
+
+		destroy: function() {
+			this._super();
+			$('.oe_leftbar').show();
+		},
+
+		start: function() {
+			var self = this;
+			new self.instance.web.Model('universal.timeline')
+        		.call('get_drivers').done(function(response) {
+        			console.log(response);
+        			if (response.status) {
+						self.$el.html(self.QWeb.render("universal_timeline_by_driver",{
+							'drivers': response.drivers,
+						}));
+						var today = new Date();
+						var date_string = self.pad(today.getFullYear(),4) + "-" + self.pad((today.getMonth()+1),2)  + "-" + self.pad(today.getDate(),2);
+						$('#filter_start_date').val(date_string);
+						$('#filter_end_date').val(date_string);
+
+					  	$('#filter_driver').on('input',function() {
+							var opt = $('option[value="'+$(this).val()+'"]');
+							self.driver_id = (opt.length ? parseInt(opt.attr('id')) : 0);
+					 	 });
+
+						self.onclick_filter_button();
+					} else {
+						alert('Server Unreachable.')
+					}
+        		});
+		},
+
+// METHODS ------------------------------------------------------------------------------------------------------------------
+
+		onclick_filter_button: function(){
+			var self = this;
+			var filter_start_date = new Date($('#filter_start_date').val());
+			var filter_end_date = new Date($('#filter_end_date').val());
+			var start_date = filter_start_date.getFullYear() + '-' + (filter_start_date.getMonth()+1) + '-' + filter_start_date.getDate();
+			var end_date = filter_end_date.getFullYear() + '-' + (filter_end_date.getMonth()+1) + '-' + filter_end_date.getDate();
+			this.render_table(self.driver_id, start_date, end_date);
+		},
+
+		render_table: function(driver_id, start_date, end_date) {
+			var self = this;
+			new this.instance.web.Model('universal.timeline')
+        		.call('get_timeline_by_driver', {
+        			 'driver_id' : driver_id,
+        			 'start_date_string' : start_date,
+        			 'end_date_string': end_date,
+        		}).done(function(response) {
+        			console.log(response);
+        			if (response.status) {
+						$('#div_main').html(self.QWeb.render("universal_timeline_by_driver_table",{
+							'driver_name': response.driver_name,
+							'dates': response.dates,
 							'hours': response.hours,
 						}));
 					} else {

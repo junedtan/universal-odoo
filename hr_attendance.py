edaf3a7c22e4fdc89036be01cc8bc9fa6c8a9199
root <root@ptuniversal.c.jovial-totality-137823.internal>
2018-07-05 09:48:49 +0000

    enable constraint attendance yang ngga ngecek sign in sign out. plus perbaikan hak akses

--- hr_attendance.py
+++ hr_attendance.py
@@ -25,10 +25,10 @@ class hr_attendance(osv.osv):
 # Si constraint checking harus clockin dlu sbelum clockout dan sebaliknya didisable dulu karena ada bug default odoo yang
 # gabisa update2 attendance yg udah lalu. Just in case stumble upon this bug again...
 # https://github.com/odoo/odoo/issues/1521
-# 	def _altern_si_so(self, cr, uid, ids, context=None):
-# 		return True
-#
-# 	_constraints = [(_altern_si_so, 'Error ! Sign in (resp. Sign out) must follow Sign out (resp. Sign in)', ['action'])]
+ 	def _altern_si_so(self, cr, uid, ids, context=None):
+ 		return True
+
+ 	_constraints = [(_altern_si_so, 'Error ! Sign in (resp. Sign out) must follow Sign out (resp. Sign in)', ['action'])]
 # ---------------------------------------------------------------------------------------------------------------------------
 
 # COLUMNS ------------------------------------------------------------------------------------------------------------------

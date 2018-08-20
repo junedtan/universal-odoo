7f6a7d91caae010dac4fa93fd59ad12e68587419
Juned <juned.tan@gmail.com>
2018-08-20 23:43:19 +0700

    ubah pemanggilan webservice_post di foms.contract supaya mem-pass browse record alih2 id-nya

--- foms_contract.py
+++ foms_contract.py
@@ -353,7 +353,7 @@ class foms_contract(osv.osv):
 		if len(user_ids) > 0:
 			user_ids = list(set(user_ids))
 			for user_id in user_ids:
-				sync_obj.post_outgoing(cr, user_id, 'foms.contract', command, contract_data.id, 
+				sync_obj.post_outgoing(cr, user_id, 'foms.contract', command, contract_data, 
 					data_columns=data_columns,
 					data_context=webservice_context)
 

73ca10dcd2a8f2d89f176a055bfd44932a82d21e
Juned <juned.tan@gmail.com>
2018-08-21 19:58:01 +0700

    debugging/update 20180821

--- foms_order.py
+++ foms_order.py
@@ -344,7 +344,10 @@ class foms_order(osv.osv):
 			# cek start date harus minimal n jam dari sekarang
 				self._cek_min_hour_for_type_by_order(cr, uid, start_date, data.customer_contract_id.by_order_minimum_minutes, context)
 				original_start_date.update({data.id: data.start_planned_date})
-				if start_date > datetime.strptime(data.finish_planned_date,'%Y-%m-%d %H:%M:%S'):
+				finish_date = vals.get('finish_planned_date', False)
+				if not finish_date: finish_date = data.finish_planned_date
+				finish_date = datetime.strptime(finish_date,'%Y-%m-%d %H:%M:%S')
+				if start_date > finish_date:
 					raise osv.except_osv(_('Order Error'),_('Start date cannot be greater than end date.'))
 		
 	# cek apakah bentrok waktu sama order lain
